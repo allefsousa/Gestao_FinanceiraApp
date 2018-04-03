cf604c25555894ee553aabb15c872b55851050f0
Allef Sousa <allefsousa_1@hotmail.com>
2018-04-03 12:44:42 -0300

    Login firebase Email e senha

---
+++ app/src/main/java/br/com/felipedeveloper/gestaofinanceira/Model/usuario.java
@@ -0,0 +1,56 @@
+package br.com.felipedeveloper.gestaofinanceira.Model;
+
+/**
+ * Created by allef on 03/04/2018.
+ */
+
+public class usuario {
+    private String usuarioNome;
+    private String usuarioEmail;
+    private String usuarioSenha;
+    private String usuarioconfirmaSenha;
+
+    public usuario() {
+    }
+
+
+    public String getUsuarioNome() {
+        return usuarioNome;
+    }
+
+    public void setUsuarioNome(String usuarioNome) {
+        this.usuarioNome = usuarioNome;
+    }
+
+    public String getUsuarioEmail() {
+        return usuarioEmail;
+    }
+
+    public void setUsuarioEmail(String usuarioEmail) {
+        this.usuarioEmail = usuarioEmail;
+    }
+
+    public String getUsuarioSenha() {
+        return usuarioSenha;
+    }
+
+    public void setUsuarioSenha(String usuarioSenha) {
+        this.usuarioSenha = usuarioSenha;
+    }
+
+    public String getUsuarioconfirmaSenha() {
+        return usuarioconfirmaSenha;
+    }
+
+    public void setUsuarioconfirmaSenha(String usuarioconfirmaSenha) {
+        this.usuarioconfirmaSenha = usuarioconfirmaSenha;
+    }
+
+    public boolean Verificasenha(String senha1,String senha2){
+        if (senha1.equals(senha2)){
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
--- app/src/main/java/br/com/felipedeveloper/gestaofinanceira/View/EmailLoginFragment.java
+++ app/src/main/java/br/com/felipedeveloper/gestaofinanceira/View/EmailLoginFragment.java
@@ -1,21 +1,78 @@
 package br.com.felipedeveloper.gestaofinanceira.View;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.v4.app.Fragment;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import br.com.felipedeveloper.gestaofinanceira.Model.usuario;
 import br.com.felipedeveloper.gestaofinanceira.R;
 
 public class EmailLoginFragment extends Fragment {
+    Button btnlogar;
+    EditText eemail;
+    EditText esenha;
+    TextView tRecuperarsenha;
+    View rootView;
+    FirebaseAuth auth;
+    usuario usuaario;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.email_login_activity,container,false);
+        rootView = inflater.inflate(R.layout.email_login_activity, container, false);
+        btnlogar = rootView.findViewById(R.id.btnentrar);
+        eemail = rootView.findViewById(R.id.editemaillogin);
+        esenha = rootView.findViewById(R.id.editsenhalogin);
+        auth = FirebaseAuth.getInstance();
+        btnlogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!usuaario.getUsuarioEmail().isEmpty() && !usuaario.getUsuarioSenha().isEmpty()){
+                    loginEmailFirebase(usuaario.getUsuarioEmail(), usuaario.getUsuarioSenha());
+                }else {
+
+                }
+
+            }
+
+        });
+
+
         return rootView;
+
+
+    }
+
+    private void loginEmailFirebase(String usuarioEmail, String usuarioSenha) {
+            auth.signInWithEmailAndPassword(usuarioEmail, usuarioSenha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        logadoComSucesso();
+                    }
+                }
+            });
+
+
+    }
+
+    private void logadoComSucesso() {
+        Intent intent = new Intent(rootView.getContext(), OpcoesFinanceiraActivity.class);
+        startActivity(intent);
     }
 
 }
